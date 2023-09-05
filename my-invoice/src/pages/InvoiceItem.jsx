063abf7aa27f22f4b696d40267f7290726ff47f9
muzamilohuthu <133840218+muzamilohuthu@users.noreply.github.com>
2023-09-05 21:55:09 +0530

    5.9

---
+++ my-invoice/src/pages/InvoiceItem.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import InvoiceField from './Invoicefield'
+
+import { BsCurrencyRupee } from "react-icons/bs";
+import { MdDeleteForever } from "react-icons/md";
+
+const InvoiceItem = ({ id, name, qty, price, onDeleteItem, onEdtiItem }) => {
+  const deleteItemHandler = () => {
+    onDeleteItem(id);
+  };
+
+  return (
+    <tr>
+      <td className="w-full">
+        <InvoiceField
+          onEditItem={(event) => onEdtiItem(event)}
+          cellData={{
+            placeholder: 'Item name',
+            type: 'text',
+            name: 'name',
+            id: id,
+            value: name,
+          }}
+        />
+      </td>
+      <td className="min-w-[65px] md:min-w-[80px]">
+        <InvoiceField
+          onEditItem={(event) => onEdtiItem(event)}
+          cellData={{
+            type: 'number',
+            min: '1',
+            name: 'qty',
+            id: id,
+            value: qty,
+          }}
+        />
+      </td>
+      <td className="relative min-w-[100px] md:min-w-[150px] ">
+      
+       <span className='realative' >
+      < BsCurrencyRupee className='absolute mt-1 ms-7' />
+      </span>
+        <InvoiceField
+        
+          onEditItem={(event) => onEdtiItem(event)}
+          cellData={{
+            className: 'text-right',
+            type: 'number',
+            min: '0.01',
+            step: '0.01',
+            name: 'price',
+            id: id,
+            value: price,
+          }}
+        />
+      </td>
+      <td className="flex items-center justify-center">
+        <button
+          className="rounded-md bg-red-500 p-2 text-white shadow-sm transition-colors duration-200 hover:bg-red-600"
+          onClick={deleteItemHandler}
+        >
+       < MdDeleteForever className='text-2xl' />
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default InvoiceItem;
